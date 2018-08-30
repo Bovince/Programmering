fd99e16a20af0bd4ae091b6274c4b5c18130db1e
Bovince <vibo1001@live.mark.se>
2018-08-30 13:59:07 +0200

    second commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+// 
+
+using namespace std;
+int main()
+{
+	
+	// cunt är standard utenhet, skärmen
+	// << är utmatningsoperator
+	
+	cout << "Programming initiated" << endl;
+	
+	// programmet körs rad efter rad
+	// fungerar allt kommer vi fram till return 0
+	// heltalet 0 returneras till operativsystemet
+	
+	cout << "Software booting" << endl;
+	cout << "_" << endl;
+	cout << "Booting completed" << endl;
+	cout << "A program by:" << endl << endl;
+	
+	cout << " BBBB     OOOO   VV       VV  II  NN    N   " << endl;
+	cout << " B   B   OO  OO   VV     VV   II  N N   N    " << endl;
+	cout << " BBBBB   O    O    VV   VV    II  N  NN N    " << endl;
+	cout << " B    B  OO  OO     VV VV     II  N   N N     " << endl;
+	cout << " BBBBB    OOOO       VVV      II  N    NN    " << endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
