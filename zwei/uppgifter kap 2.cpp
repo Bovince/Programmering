250f60690f5890a34480fa2c7780d01f2cd65d68
Bovince <vibo1001@live.mark.se>
2018-09-20 14:55:50 +0200

    commit fümf

---
+++ zwei/uppgifter kap 2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+//uppgift 2,1
+/*
+{
+	float a, b, c, d;
+	
+	cin >> a;
+	cout << endl;
+	cin >> b;
+	cout << endl;
+	cin >> c;
+	cout << endl;
+	
+	cout << endl;
+	
+	d=a+b-c;
+	
+	cout << a; cout << "+"; cout << b; cout << "-"; cout << c; cout << "="; cout << d;
+	
+	return 0;
+}
+*/
+/*
+{
+	int tal;
+	double storttal;
+	char bokstav;
+	cout << sizeof(char) << endl;
+	tal = 1.234567890;
+	//storttal = Hej;
+	bokstav = 72;
+	
+	cout << tal; cout << endl;
+	//cout << storttal; cout << endl;
+	cout << bokstav; cout << endl;
+	
+	return 0;
+}
+*/
+
+{
+	int ein;
+	cin >> ein; cout << endl;
+	cout << (char)ein;
+	
+	return 0;
+}
