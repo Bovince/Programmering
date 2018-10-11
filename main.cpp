a39dc12cbd1c48b3074942c5f96af224bc6ea936
Bovince <vibo1001@live.mark.se>
2018-10-11 15:03:39 +0200

    commit 9

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream> 
+#include <string>
+#include <math.h>
 
 using namespace std;
 int main()
@@ -27,7 +29,7 @@ int main()
 	
 	cout << "Choose action" << endl << endl;
 	cout << "1. Play snake" << endl;
-	cout << "2. Go to weebsite" << endl;
+	cout << "2. Go to website" << endl;
 	cout << "3. Use calculator" << endl << endl;
 	
 	int action;
@@ -52,20 +54,10 @@ int main()
 
 
 
-
-
-
 	if (action==2)
 	{
-		cout << "localhost/~Bovince"; 
+		system("</opt/google/chrome/chrome> https://google.se");
 	}
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
@@ -76,8 +68,8 @@ int main()
 	if (action==3)
 	{
 		
-	int method , tal1 , tal2 , tal3;
-	
+	int method;
+	float tal1 , tal2 , tal3 , tal4;
 	char fortsatt='y';
 	
 	while (fortsatt=='y')
@@ -92,15 +84,21 @@ int main()
 	cout << "1:Addition" << endl;
 	cout << "2:Subtraction" << endl;
 	cout << "3:Multiplication" << endl;
-	cout << "4:Division" << endl << endl;
+	cout << "4:Division" << endl;
+	cout << "5:Power" << endl;
+	cout << "6:Squarerot" << endl;
+	cout << "7:Solve equation" << endl << endl;
 	
 	cin >> method;
 	
 	cout << endl;
 	
 	
+	
+	
 	if (method==1)
 	{
+		
 		cout << "Enter term" << endl;
 	cin >> tal1;
 	
@@ -123,13 +121,9 @@ int main()
 	
 	
 	
-	
-	
-	
-	
-	
 	if (method==2)
 	{
+		
 		cout << "Enter term" << endl;
 	cin >> tal1;
 	
@@ -154,14 +148,9 @@ int main()
 	
 	
 	
-	
-	
-	
-	
-	
-	
 	if (method==3)
 	{
+		
 		cout << "Enter factor" << endl;
 	cin >> tal1;
 	
@@ -186,15 +175,9 @@ int main()
 	
 	
 	
-	
-	
-	
-	
-	
-	
 	if (method==4)
 	{
-		float tal3 , tal2;
+		
 		cout << "Enter numerator" << endl;
 	cin >> tal1;
 	
@@ -218,14 +201,154 @@ int main()
 	
 	
 	
+	if (method==5)
+	{
+		
+		cout << "Enter term" << endl;
+	cin >> tal1;
+	
+	cout << endl;
+	cout << "Enter power" << endl; 
+	cin >> tal2;
+	
+	cout << endl;
+	
+	cout << tal1; cout <<"^"; cout << tal2; cout << endl;
+	
+	cout << endl;
+	
+	tal3=pow(tal1 , tal2);
+	
+	cout << tal3;
+	
+	}
 	
 	
 	
 	
+	
+	if (method==6)
+	{
+		
+		cout << "Enter number" << endl;
+	cin >> tal1;
+	
+	cout << endl;
+	
+	cout <<"√"; cout << tal1; cout << "=";
+	
+	tal3=sqrt(tal1);
+	
+	cout << tal3;
+	
+	}
+	
+	
+	
+	
+	
+	if (method==7)
+	{
+		int type;
+		cout << "Choose equation type" << endl;
+		cout << "1:x+y" << endl;
+		cout << "2:x-y" << endl;
+		cout << "3:x²+yx+z" << endl;
+		cout << "4:x²-yx+z" << endl;
+		cout << "5:x²+yx-z" << endl;
+		cout << "6:x²-yx-z" << endl;
+		cout << endl;
+		
+		cin >> type;
+		
+			if (type==1)
+			{	
+				cout << endl;
+				cout << endl;
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				cout << endl;
+				
+				char unknown;
+				
+				cin >> unknown;
+				
+					if (unknown=='x')
+					{
+						cout << "Please input y" << endl;
+						cin >> tal1;
+						cout << endl;
+						cout << "Please input summ" << endl;
+						cin >> tal2;
+						cout << endl;
+						tal3=tal2-tal1;
+						cout << "Your unknown is" << tal3;
+					}
+					
+					if (unknown=='y')
+					{
+						cout << "Please input x" << endl;
+						cin >> tal1;
+						cout << endl;
+						cout << "Please input summ" << endl;
+						cin >> tal2;
+						cout << endl;
+						tal3=tal2-tal1;
+						cout << "Your unknown is " << tal3;	
+					}
+				
+			}
+			
+			if (type==2)
+			{
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				
+			}
+			
+			if (type==3)
+			{
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				
+			}
+			
+			if (type==4)
+			{
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				
+			}
+			
+			if (type==5)
+			{
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				
+			}
+			
+			if (type==6)
+			{
+				cout << "Input values x or y and the summ on request" << endl;
+				cout << "But first choose unknown term" << endl;
+				cout << "x or y" << endl;
+				
+			}
+			
+	
+	}
+	
+	cout << endl;
+	cout << endl;
 	cout << endl;
 	cout << endl;
 	cout << "Do you wish to count again"; cout << endl;
-	cout << "If so press \"y\""; cout << endl;
+	cout << "If so press \"y\", else press \"any\" buton but \"y\""; cout << endl;
 	
 	cin >> fortsatt;
 }
---
+++ operatorer/ein.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hej" << endl;
+	int tal;
+	tal = ((4*2)+1);	// paranteser krävs inte
+	cout << tal << endl;
+	
+	int i=1;
+	i=i+1;
+	i+=1;
+	i++;
+	cout << i;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	}
