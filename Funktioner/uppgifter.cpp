ab9b58ac1fc81fc0cdce32ab6a6da111a7ca7cfa
Bovince <vibo1001@live.mark.se>
2019-04-11 13:08:02 +0200

    commit 25

---
+++ Funktioner/uppgifter.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+/*
+ * Skriv ett program som använder en funktion för att multiplicera alla tal i ett int-fält med -1
+ * 
+void multiplicering(int falt[]);
+int main()
+{
+	int falt[4];
+	for (int n=0; n<4; n++)
+	{
+		
+		cin >> falt[n];
+		
+	}
+	multiplicering (falt);
+	
+	for (int n=0; n<4; n++)
+	{
+		
+		cout << falt[n] << ' ';
+		
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
+void multiplicering(int falt[])
+{
+	
+	for (int n=0; n<4; n++)
+	{
+		
+		falt[n]=(falt[n]*-1);
+		
+	}
+	
+}
+
+*/
+
+/*
+
+//Skriv ett program som tar ett fält med double som argument och returnerar det högsta talet som finns i fältet
+
+double hitta(double falt[]);
+int x;
+double storsta=0;
+int main()
+{
+	cout << "Hur många fält vill du ha?";
+	
+	cin >> x;
+	
+	double falt[100]; 
+	
+	for (int n=0; n<100; n++)
+	{
+		cin >> falt[n];
+		
+		if (n>=x-1)
+		{
+			for (int n=x; n<100; n++)
+				falt[n]=0;
+			break;
+		}
+		
+	}
+	
+	hitta(falt);
+	
+	cout << storsta;
+	
+	return 0;
+}
+
+double hitta(double falt[])
+{
+	
+	for (int n=0; n<100; n++)
+	{
+		if(storsta < falt[n])
+		{
+		storsta = falt[n];
+		}
+	}
+	return storsta;
+}
+
+*/
+
+
+
+
+
+
+
--- problemlösning/Kängurun.cpp
+++ problemlösning/Kängurun.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int x, y, z;
+int x, y, z, totalVikt;
 int funktion(int& z);
 int main()
 {
@@ -11,14 +11,17 @@ int main()
 	
 	cout << endl << "Y=" << y << endl;
 	
-	cout << "Mamman har många barn och ska nu bära med sig några. De första två väger X gram. Bestäm X:";
+	cout << "Mamman har många barn och ska nu bära med sig några." << endl << "De första två väger X gram. Bestäm X:";
 	
 	cin >> x;
 	
 	cout << endl << "X=" << x << endl;
 	
-	cout << "Varje barn efter de två första väger lika mycket som de två föregående." << endl << "Mamman kommer därför kunna bära " << funktion(z) << " st av sina barn med sig";
+	cout << "Varje barn efter de två första väger lika mycket som de två föregående." << endl;
 	
+	cout << "Mamman kommer därför kunna bära " << funktion(z) << " av sina barn med sig" << endl;
+	
+	//cout << "Totalvikten blir " << totalVikt << endl;
 	
 	return 0;
 	
@@ -28,28 +31,43 @@ int main()
 int funktion(int& z)
 {
 	int barn[100];
-	int totalVikt = 0;
+	
 	
 	barn[0]=x;
 	barn[1]=x;
+	z=0;
+	totalVikt=0;
+	
+	if (y > totalVikt)
+		{
+			totalVikt=totalVikt+barn[0];
+			
+			z++;
+		}
+		if (y > totalVikt)
+		{
+			totalVikt=totalVikt+barn[1];
+			
+			z++;
+		}
 	
 	for (int n=2; n<103; n++)
 	{
 		barn[n]=barn[n-1]+barn[n-2];
-		z=n-1;
-		if (y > totalVikt)
+	
+		if (y >= totalVikt)
 		{
 			totalVikt=totalVikt+barn[n];
+			
+			z++;
 		}
 		else
 		{
+			z--;
 		break;
 		}
 	}
-	if (z==2 && totalVikt==x)
-	{	
-		z--;
-	}
 	
 	return z;
+	
 }
